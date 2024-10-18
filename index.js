96bca5523e7ec05bdfb65a6f3892dea96a610951
TonyChow1223 <1107965280@qq.com>
2024-10-18 13:02:12 +0800

    更新

--- index.js
+++ index.js
@@ -340,7 +340,7 @@ option2 = {
             },
             symbol: 'square',
             data: [
-                [225000, 1600000, 7.1, "sample68"]
+                [225000, 1600000, 7.1, "sample68", "https://ieeexplore.ieee.org/document/10213232"]
             ],
             visible: true
         },
